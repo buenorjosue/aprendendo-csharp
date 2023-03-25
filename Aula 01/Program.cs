20bbc394d94fce685b141e17980629f3e7497619
josuebuenor <josuebuenor@ufrrj.br>
2023-03-24 23:11:38 -0300

    Aula 01

---
+++ Aula 01/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); // Escreve o Hello World no console
+Console.ReadKey(); //Espera o input para continuar
